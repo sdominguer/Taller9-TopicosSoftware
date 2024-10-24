3614ff9a8de1351b8278a38a65f244ccb11c3b6d
sdominguer <88257827+sdominguer@users.noreply.github.com>
2024-10-24 01:20:43 -0500

    Update randomquotes.py

--- randomquotes-flask-master/randomquotes.py
+++ randomquotes-flask-master/randomquotes.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 import random
+import os
 
 app = Flask(__name__)
 
@@ -15,11 +15,11 @@ phrases = [
 ]
 
 
-@app.route('/')
-def get_random_quote():
-    phrase = random.choice(phrases)
-    container_id = os.uname()[1] 
-    return f"{phrase} - Container Id: {container_id}"
+@app.route('/') 
+def get_random_quote(): 
+    phrase = random.choice(phrases) 
+    container_id = os.uname()[1]  
+    return f"{phrase} - Container Id: {container_id}" 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
